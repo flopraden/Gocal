ad6534a955a7fbe323c27a2c1e9bd125b61a192e
fpraden@Tauri <fpraden@Tauri>
2024-11-23 13:34:14 +0100

    * [flake] + correct vendor hash after update

--- flake.nix
+++ flake.nix
@@ -46,7 +46,7 @@
             # remember to bump this hash when your dependencies change.
             # vendorHash = pkgs.lib.fakeHash;
 
-            vendorHash = "sha256-pQpattmS9VmO3ZIQUFn66az8GSmB4IvYhTTCFn6SUmo=";
+            vendorHash = "sha256-T03Ap+xvDybWbIyunjLsStb8rsTM8NaVdNHMrqsA57U=";
           };
         });
 
